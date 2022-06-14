8397c361f2a176ce3568d39a9348507a26f6600a
TobiPerel <pereltobias@gmail.com>
2022-06-13 23:16:45 -0300

    database

--- spotify/musica.py
+++ spotify/musica.py
@@ -6,8 +6,7 @@ print(cancionesYArtistas)
 def agregarCancion(cancion,artista):
     canciones = list(cancionesYArtistas.values())
     if cancion not in canciones:
-        canciones.append(cancion)
         cancionesYArtistas[artista] = cancion
-    print(cancionesYArtistas)
-    insertarCancion(cancion,artista)
+        print(cancionesYArtistas)
+        insertarCancion(cancion,artista)
 
