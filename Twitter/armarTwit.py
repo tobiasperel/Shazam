eb22f19f7c49b983f645e3cd1da302661659f9a2
TobiPerel <pereltobias@gmail.com>
2022-06-30 23:31:57 -0300

    solucionar error armarwtit

--- Twitter/armarTwit.py
+++ Twitter/armarTwit.py
@@ -4,7 +4,7 @@ def armarTwit(cancion, artista):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
     hora = str(int(current_time.split(":")[0])-3)
-    if hora < 0:
+    if int(hora) < 0:
         hora +=  3
     current_time.split(":")[0] = hora
     current_time = hora + ":" + current_time.split(":")[1] + ":" + current_time.split(":")[2]
