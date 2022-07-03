5bd7951993064144b00d060fd67a1b3dd263f34e
tobiasperel <pereltobias@gmail.com>
2022-07-03 22:51:28 +0000

    database

--- Twitter/armarTwit.py
+++ Twitter/armarTwit.py
@@ -1,12 +1,8 @@
 from datetime import datetime
 
+
 def armarTwit(cancion, artista):
     now = datetime.now()
     current_time = now.strftime("%H:%M:%S")
-    hora = str(int(current_time.split(":")[0])-3)
-    if int(hora) < 0:
-        hora +=  3
-    current_time.split(":")[0] = hora
-    current_time = hora + ":" + current_time.split(":")[1] + ":" + current_time.split(":")[2]
     text = "Hoy, a las " + current_time + ", La cancion " + cancion + " del artista " + artista + " sono en Aspen."
     return text
